2d1db988611e98bdda3e18c8c9f363bcc85d53b1
Ahmed Moalim <ahmedm23@uw.edu>
2017-04-13 14:02:54 -0700

    Uploaded Files

---
+++ DFlipFlop.v
@@ -0,0 +1,15 @@
+module DFlipFlop(clk, reset, D, q, qBar);
+   input D, clk, reset;
+   output q, qBar;
+   reg q;
+
+   not n1 (qBar, q);
+
+   always @ (negedge reset or posedge clk)
+      begin
+         if(!reset)
+            q = 0;
+         else
+            q = D;
+      end
+endmodule
