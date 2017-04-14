3a2b0f3d03f47a3072c82d8f710ff665103d5841
radleigh <radleigh@msn.com>
2017-04-14 15:00:50 -0700

    Fixed syncro to compile

--- Lab1/syncrocounter.v
+++ Lab1/syncrocounter.v
@@ -3,7 +3,7 @@
 	//b. Use the D flip-flop model in the adjacent listing.
 module syncrocounter (clk, reset, next);
 	input clk, reset;
-	wire [3:0] curr;
+	wire [3:0] curr, qBar;
 	output [3:0] next;
 	
 	assign next[0] = ~curr[0];
@@ -11,10 +11,10 @@ module syncrocounter (clk, reset, next);
 	assign next[2] = curr[2]^(curr[1] & curr[0]);
 	assign next[3] = curr[3]^(curr[2] & curr[1] & curr[0]);
 	
-	DFlipFlop bit0(.clk(clk), .reset(reset), .D(curr[0]), .q(next[0]), .qBar(0));
-	DFlipFlop bit1(.clk(clk), .reset(reset), .D(curr[1]), .q(next[1]), .qBar(0));
-	DFlipFlop bit2(.clk(clk), .reset(reset), .D(curr[2]), .q(next[2]), .qBar(0));
-	DFlipFlop bit3(.clk(clk), .reset(reset), .D(curr[3]), .q(next[3]), .qBar(0));
+	DFlipFlop bit0(.clk(clk), .reset(reset), .D(next[0]), .q(curr[0]), .qBar(qBar[0]));
+	DFlipFlop bit1(.clk(clk), .reset(reset), .D(next[1]), .q(curr[1]), .qBar(qBar[0]));
+	DFlipFlop bit2(.clk(clk), .reset(reset), .D(next[2]), .q(curr[2]), .qBar(qBar[0]));
+	DFlipFlop bit3(.clk(clk), .reset(reset), .D(next[3]), .q(curr[3]), .qBar(qBar[0]));
 endmodule
 
 
