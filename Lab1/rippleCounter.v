2485ea800c4d3cbffc960cf2a1bd8a2fd712a41f
atangzwj <atangzwj@outlook.com>
2017-04-18 15:49:00 -0700

    Commented rippleCounter.v

--- Lab1/rippleCounter.v
+++ Lab1/rippleCounter.v
@@ -1,13 +1,12 @@
-`include "DFlipFlop.v"
 
-// A four stage (4 bit) ripple up (asynchronous) counter, with active low reset.
-//a. Design the counter using a gate or structural model.
-//b. Use the D flip-flop model in the adjacent listing.
 module rippleCounter(clk, reset, count);
    input clk, reset;
    output [3:0] count;
    wire   [3:0] countbar;
-	
+
+   // bit0 to bit3's outputs are the lowest to highest bit of the count
+   // clk goes to lowest bit's DFF
+   // qBar of each bit goes to clk of the next bit up
    DFlipFlop bit0(.clk(clk), .reset(reset), .D(countbar[0]), .q(count[0]), .qBar(countbar[0]));
    DFlipFlop bit1(.clk(countbar[0]), .reset(reset), .D(countbar[1]), .q(count[1]), .qBar(countbar[1]));
    DFlipFlop bit2(.clk(countbar[1]), .reset(reset), .D(countbar[2]), .q(count[2]), .qBar(countbar[2]));
