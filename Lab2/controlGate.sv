6a72683e3fa31c090eb7b18bfb5267520b410794
Ahmed Moalim <ahmedm23@uw.edu>
2017-04-28 16:25:56 -0700

    more syntax

--- Lab2/controlGate.sv
+++ Lab2/controlGate.sv
@@ -5,11 +5,11 @@ module controlGate (clk, reset, upper_switch, lower_switch, gate_state, water_st
    
    output reg [1;0] gate_state;
 
-   reg [2:0] ns;
+   reg [2:0] ps, ns;
 
    // Next state logic
    always_comb
-      case(gate_state)
+      case(ps)
          2'b00: if (~occupied & upper_switch & water_status == 2'b11)     ns = 2'b01;
                else if (~occupied & lower_switch & water_status == 2'b10) ns = 2'b10;
                else if (occupied & upper_switch & water_status == 2'b10)  ns = 2'b10  
@@ -18,12 +18,14 @@ module controlGate (clk, reset, upper_switch, lower_switch, gate_state, water_st
          default:                                                         ns = 2'b00; 
       endcase 
    
+   assign gate_state = ps;
+
    // DFFs
    always_ff @(posedge clk)
    begina
       if (reset)
-         gate_state <= 2'b00;
+         ps <= 2'b00;
       else
-         gate_state <= ns;
+         ps <= ns;
    end
 endmodule
