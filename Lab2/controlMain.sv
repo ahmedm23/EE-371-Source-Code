3ab12eee5ce3ce22a67ff18fd29dcb5a4e4d4a89
radleigh <radleigh@msn.com>
2017-04-30 19:45:51 -0700

    modules in controlMain, added secondGate

--- Lab2/controlMain.sv
+++ Lab2/controlMain.sv
@@ -1,5 +1,5 @@
-module controlMain(clk, reset, arr_sw, dep_sw, gate1_sw, gate2_sw,
-                   w_up, w_down, arr_li, dep_li, gate1_li, gate2_li);
+module controlMain (clk, reset, arr_sw, dep_sw, gate1_sw, gate2_sw,
+                    w_up, w_down, arr_li, dep_li, gate1_li, gate2_li);
 //  Arrival Switch SW0 -- Arrival Light LEDR0
 //  Depart  Switch SW1 -- Depart Light  LEDR1
 //  1stGate Switch Sw2 -- 1stGate Light LEDR2  
@@ -9,53 +9,59 @@ module controlMain(clk, reset, arr_sw, dep_sw, gate1_sw, gate2_sw,
 
 //  reset KEY0
    
-   arrival a (.clk, .reset, .arr_sw, arr_li);  
+   arrivalLight a (.clk, .reset, .arr_sw, arr_li);  
 // Arrival Module that Ahmed will do 
-Input: Clk, reset, SW0 
-Output: LEDR0, (count of timer)
-   if SW = 1
-   After 5 min delay 
-   Turn on LEDR0
+//Input: Clk, reset, SW0 
+//Output: LEDR0, (count of timer)
+//   if SW = 1
+//   After 5 min delay 
+//   Turn on LEDR0
    
    logic water_level [5:0];
 
    controlWater c (.clk, .reset, .w_up, .w_down, .water_level);
 // Waterlevel Module
-Input: clk, reset, KEY1, KEY2
-Logic: Waterlevel
-Output: Waterlevel [5:0] 
-   Takes in key presses and adjusts water level accordingly
+//Input: clk, reset, KEY1, KEY2
+//Logic: Waterlevel
+//Output: Waterlevel [5:0] 
+//   Takes in key presses and adjusts water level accordingly
 
-   firstGate g (.clk, .reset, .gate1_sw   
+   logic exited, occupied;
+   
+   firstGate G1 (.clk, .reset, .gate1_sw, .water_level, .arr_li, .exited,
+                .gate1_li, .occupied);
 // Allow Boat to Enter 1st Gate Module
-Input: clk, reset, SW2, Waterlevel, LEDR0, exited
-Output: LEDR2, Occupied
-   A: if waterlevel > 47 && LEDR0 = 1 && SW2 = 1 // if the two conditions are met and gate switch is high
-         Go to B  
-      else
-         Stay in A
-   B: If SW2 is flipped down = 0
-      Go to state C
-   C: if exited = 1 
-      Go to state A
-   assign LEDR2 = (ps == B); 
-   assign Occupied = (ps == C);
-   
-   
-// Allow Boat to Exit 2nd Gate Module
-Input: clk, reset, SW1, SW3, waterlevel, Occupied,  
-Output: LEDR1, LEDR3, exited  
-   A: if occupied and SW1 = 1
-      go to B
-   B: if waterlevel < 3 && SW3 = 1
-      go to C
-   C: if SW1 = 0 && SW3 = 0
-      go to D
-   D: go to state A
-   assign LEDR1 = (SW1 == 1 && occupied); //Depart light one when depart switch is high and Lock is occupied
-   assign LEDR3 = (ps == C);
-   assign exited = (ps == D);
+//Input: clk, reset, SW2, Waterlevel, LEDR0, exited
+//Output: LEDR2, Occupied
 
+
+//   A: if waterlevel > 47 && LEDR0 = 1 && SW2 = 1 // if the two conditions are met and gate switch is high
+//         Go to B  
+//      else
+//         Stay in A
+//   B: If SW2 is flipped down = 0
+//      Go to state C
+//   C: if exited = 1 
+//      Go to state A
+//   assign LEDR2 = (ps == B); 
+//   assign Occupied = (ps == C);
+  
+   secondGate G2 (.clk, .reset, .dep_sw, .gate2_sw, .water_level, .occupied, 
+                  .dep_li, .gate2_li, .exited); 
+                  
+// Allow Boat to Exit 2nd Gate Module
+//Input: clk, reset, SW1, SW3, waterlevel, Occupied,  
+//Output: LEDR1, LEDR3, exited  
+//   A: if occupied and SW1 = 1
+//      go to B
+//   B: if waterlevel < 3 && SW3 = 1
+//      go to C
+//   C: if SW1 = 0 && SW3 = 0
+//      go to D
+//   D: go to state A
+//   assign LEDR1 = (SW1 == 1 && occupied); //Depart light one when depart switch is high and Lock is occupied
+//   assign LEDR3 = (ps == C);
+//   assign exited = (ps == D);
 
   
 endmodule
---
+++ Lab2/secondGate.sv
@@ -0,0 +1,93 @@
+// Allow Boat to Exit 2nd Gate Module
+module secondGate (clk, reset, dep_sw, gate2_sw, water_level, occupied, 
+                   dep_li, gate2_li, exited);
+	input clk, reset, dep_sw, gate2_sw, occupied;
+   input [5:0] water_level;
+   output dep_li, gate2_li, exited; 
+   
+	enum { A, B, C, D} ps, ns;
+   
+   always_comb
+      case(ps)
+         A: if (occupied & dep_sw)           ns = B;
+            else                             ns = B;
+         B: if (water_level < 3 & gate2_sw)  ns = C;
+            else                             ns = B;
+         C: if (~dep_sw & ~gate2_sw)         ns = D;
+            else                             ns = C;
+         D:                                  ns = A;
+      endcase
+   assign dep_li   = (dep_sw & occupied); //Depart light one when depart switch is high and Lock is occupied
+   assign gate2_li = (ps == C);
+   assign exited   = (ps == D);
+   
+//Input: clk, reset, SW1, SW3, waterlevel, Occupied,  
+//Output: LEDR1, LEDR3, exited  
+//   A: if occupied and SW1 = 1
+//      go to B
+//   B: if waterlevel < 3 && SW3 = 1
+//      go to C
+//   C: if SW1 = 0 && SW3 = 0
+//      go to D
+//   D: go to state A
+
+   always @(posedge clk)
+	begin
+		if (reset)
+			ps <= A;
+		else
+			ps <= ns;
+	end
+   
+endmodule
+
+module secondGateTestBench();
+   logic clk, reset, dep_sw, gate2_sw, occupied;
+   logic [5:0] water_level;
+   logic dep_li, gate2_li, exited;
+   
+   secondGate test (.clk, .reset, .dep_sw, .gate2_sw, .water_level, .occupied, .dep_li, .gate2_li, .exited);
+   
+   parameter clk_PERIOD=50;
+   initial begin
+      clk <= 0;
+      forever #(clk_PERIOD) clk <= ~clk;
+   end
+
+   initial begin
+                                                   @(posedge clk);
+   reset <= 1;                                     @(posedge clk);
+   reset <= 0; occupied <= 0; dep_sw <= 0;                         @(posedge clk);
+                                                   @(posedge clk);
+                                       @(posedge clk);
+               occupied <= 1;                                    @(posedge clk);
+                                                   @(posedge clk);
+               dep_sw <= 1;                                    @(posedge clk);
+                                                   @(posedge clk);
+                                                   @(posedge clk);
+               water_level <= 6'd2; gate2_sw <= 1;                                     @(posedge clk);
+                                                   @(posedge clk);
+                                                   @(posedge clk);
+                                                   @(posedge clk);
+               gate2_sw <= 0; dep_sw <= 0;                                     @(posedge clk);
+                                                   @(posedge clk);
+                                                   @(posedge clk);
+                                                   @(posedge clk);
+                                                   @(posedge clk);
+                                                   @(posedge clk);
+                                                   @(posedge clk);
+                                                   @(posedge clk);
+                                                   @(posedge clk);
+                                                   @(posedge clk);
+                                                   @(posedge clk);
+                                                   @(posedge clk);
+                                                   @(posedge clk);
+                                                   @(posedge clk);
+                                                   @(posedge clk);
+                                                   @(posedge clk);
+                                                   @(posedge clk);
+                                                   @(posedge clk);
+
+   //$stop; // End the simulation.
+   end
+endmodule
