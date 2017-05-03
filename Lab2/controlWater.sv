1f075a561636a19cfa82abcd358e7910a2f3a7d3
atangzwj <atangzwj@outlook.com>
2017-05-02 17:29:51 -0700

    Revised controlWater.sv, fixed behavior of water_low flag

--- Lab2/controlWater.sv
+++ Lab2/controlWater.sv
@@ -30,8 +30,8 @@ module controlWater (clk, reset, water_high, water_low, w_up, w_down);
 
       // Timing for water to go 0' -> 4.7'
       // Timing for water to go 5' -> 0.3'
-      water_high = water_counter > 7'd74 || ps == high;
-      water_low = water_counter > 7'd65 || ps == low;
+      water_high = water_counter > 7'd74 | ps == high;
+      water_low = (water_counter > 7'd65 && ps == lower) | ps == low;
    end
 
    always_ff @ (posedge clk)
