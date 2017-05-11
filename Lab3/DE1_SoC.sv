3662e5facecc35d7c4b0d3ad59c9f7a750341026
radleigh <radleigh@msn.com>
2017-05-11 13:33:01 -0700

    revised comments to de1soc

--- Lab3/DE1_SoC.sv
+++ Lab3/DE1_SoC.sv
@@ -16,23 +16,17 @@ module DE1_SoC (CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW);
 	clk 							tBase[parameter]
 	reset 							KEY[0]
 	
-	scanning_s1 					LEDR[9]
-	ready_trans_s1 					LEDR[8]
-	standby_s1 						LEDR[7]
-	idle_s1 						LEDR[6]
-	hex_display_s1 					HEX5
+    hex_state_s1					HEX5
+	hex_count_s1 					HEX4
 	
 	transfer 
 	
 	FOR SCANNER 2 -------------------------
-	clk tBase[parameter]
-	reset KEY[0]
+	clk 							tBase[parameter]
+	reset 							KEY[0]
 	
-	scanning_s2 					LEDR[4]
-	ready_trans_s2					LEDR[3]
-	standby_s2						LEDR[2]
-	idle_s2							LEDR[1]
-	hex_display_s2 					HEX0
+	hex_state_s2					HEX1
+	hex_count_s2 					HEX0
 	
 	transfer
 	*/
