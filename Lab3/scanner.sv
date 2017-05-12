099fe77bfd1e82e6389698a173d5d8d7380dba7e
atangzwj <atangzwj@outlook.com>
2017-05-12 16:31:14 -0700

    First draft of combinational logic in scanner module completed

--- Lab3/scanner.sv
+++ Lab3/scanner.sv
@@ -7,41 +7,78 @@ module primScanner (clk, reset, scan_status, rdy_flush, start_scan_out,
    output logic [7:0] mem_used;
    input  logic       start_scan_in, flush;
    input  logic [7:0] alt_mem_used;
-   logic              scan;
+   logic              scan, flush_mem;
 
-   memory buf (.clk, .reset, .mem_used, .scan, .flush);
+   memory mem (.clk, .reset, .mem_used, .scan, .flush(flush_mem));
 
    enum {low_pwr, stby, idle, scanning, flushing} ps, ns;
 
-   always_comb
+   always_comb begin
       case (ps)
-         scanning: if (mem_used < 80) begin
+         scanning: if (mem_used < 8'd80) begin
                       ns = scanning; // Buffer < 80, keep scanning
                       rdy_flush = 0;
                       goto_stby_out = 0;
                       start_scan_out = 0;
                    end
-                   else if (mem_used < 90) begin
+                   else if (mem_used < 8'd90) begin
                       if (flush) ns = flushing;
                       else       ns = scanning;
                       rdy_flush = 1;      // 80% full, keep scanning, but ready
                       goto_stby_out = 1;  // to transfer, signal other scanner 
                       start_scan_out = 0; // to go to stby
                    end
-                   else if (mem_used < 100) begin
-                      ns = scanning;     // 90% full, keep scanning, still ready
-                      rdy_flush = 1;     // to transfer, signal other scanner to
-                      goto_stby_out = 0; // start scanning
-                      start_scan_out = 1;
+                   else if (mem_used < 8'd100) begin
+                      if (flush) ns = flushing;
+                      else       ns = scanning;
+                      rdy_flush = 1;      // 90% full, keep scanning, still
+                      goto_stby_out = 0;  // ready to transfer, signal other
+                      start_scan_out = 1; // scanner to start scanning
                    end
                    else begin // 100% full
-                      ns = 
+                      if (flush | alt_mem_used > 8'd49) ns = flushing;
+                      else                              ns = idle;
+                      rdy_flush = 1;
+                      goto_stby_out = 0;
+                      start_scan_out = 1;
+                   end
+         flushing: begin 
+                      if (mem_used > 8'd0) ns = flushing;
+                      else                 ns = low_pwr;
+                      rdy_flush = 0;
+                      goto_stby_out = 0;
+                      start_scan_out = 0;
+                   end
+         low_pwr:  begin
+                      if (goto_stby_in) ns = stby;
+                      else              ns = low_pwr;
+                      rdy_flush = 0;
+                      goto_stby_out = 0;
+                      start_scan_out = 0;
+                   end
+         stby:     begin
+                      if (start_scan_in) ns = scanning;
+                      else               ns = stby;
+                      rdy_flush = 0;
+                      goto_stby_out = 0;
+                      start_scan_out = 0;
+                   end
+         idle:     begin
+                      if (flush) ns = flushing;
+                      else       ns = idle;
+                      rdy_flush = 0;
+                      goto_stby_out = 0;
+                      start_scan_out = 0;
+                   end
+
+      flush_mem = ps == flushing;
+   end
 
    always_ff @ (posedge clk)
       if (reset)
-         ps = scanning;
+         ps <= scanning;
       else
-         ps = ns;
+         ps <= ns;
 
 
 // 3 bit state output
