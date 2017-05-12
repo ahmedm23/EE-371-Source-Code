487e7a057bfc6881b05e547f5ab43299dbd6788b
atangzwj <atangzwj@outlook.com>
2017-05-12 15:33:38 -0700

    Revised memory.sv implementation (no transfer signal, only flush)

--- Lab3/memory.sv
+++ Lab3/memory.sv
@@ -1,24 +1,24 @@
 
-module memory (clk, reset, mem_used, scan, transfer);
-   input  logic clk, reset;
+module memory (clk, reset, mem_used, scan, flush);
+   input  logic       clk, reset;
    output logic [7:0] mem_used;
-   input  logic scan, transfer;
+   input  logic       scan, flush;
 
    always_ff @ (posedge clk)
-      if (reset)                   mem_used <= 8'd0;
-      else if (scan & mem_used < 8'd100)   mem_used <= mem_used + 8'd1;
-      else if (transfer & mem_used > 8'd2) mem_used <= mem_used - 8'd3;
-      else if (transfer & mem_used > 8'd0) mem_used <= mem_used - 8'd1;
-      else                                 mem_used <= mem_used;
+      if (reset)                         mem_used <= 8'd0;
+      else if (scan & mem_used < 8'd100) mem_used <= mem_used + 8'd1;
+      else if (flush & mem_used > 8'd2)  mem_used <= mem_used - 8'd3;
+      else if (flush & mem_used > 8'd0)  mem_used <= mem_used - 8'd1;
+      else                               mem_used <= mem_used;
 
 endmodule
 
 module memory_testbench ();
    logic clk, reset;
    logic [7:0] mem_used;
-   logic scan, transfer, flush;
+   logic scan, flush;
 
-   memory dut (.clk, .reset, .mem_used, .scan, .transfer, .flush);
+   memory dut (.clk, .reset, .mem_used, .scan, .flush);
 
    parameter CLK_PER = 10;
    initial begin
@@ -28,31 +28,31 @@ module memory_testbench ();
 
    integer i;
    initial begin
-                                                        @(posedge clk);
-      reset <= 1; scan <= 0; transfer <= 0; flush <= 0; @(posedge clk);
-      reset <= 0;                                       @(posedge clk);
-                                                        @(posedge clk);
-                                                        @(posedge clk);
-                  scan <= 1;                            @(posedge clk);
-      for (i = 0; i < 108; i++) begin
-         @(posedge clk);
-      end
-                  scan <= 0;                            @(posedge clk);
-                                                        @(posedge clk);
-                             transfer <= 1;             @(posedge clk);
-      for (i = 0; i < 60; i++) begin
-         @(posedge clk);
-      end
-                             transfer <= 0;             @(posedge clk);
-                                                        @(posedge clk);
-                  scan <= 1;                            @(posedge clk);
-      for (i = 0; i < 10; i++) begin
-         @(posedge clk);
-      end
-                  scan <= 0;                            @(posedge clk);
-                                            flush <= 1; @(posedge clk);
-                                            flush <= 0; @(posedge clk);
-                                                        @(posedge clk);
+                                         @(posedge clk);
+      reset <= 1; scan <= 0; flush <= 0; @(posedge clk);
+      reset <= 0;                        @(posedge clk);
+                                         @(posedge clk);
+                                         @(posedge clk);
+                  scan <= 1;             @(posedge clk);
+
+      for (i = 0; i < 108; i++)          @(posedge clk);
+
+                  scan <= 0;             @(posedge clk);
+                                         @(posedge clk);
+                             flush <= 1; @(posedge clk);
+
+      for (i = 0; i < 60; i++)           @(posedge clk);
+
+                             flush <= 0; @(posedge clk);
+                                         @(posedge clk);
+                  scan <= 1;             @(posedge clk);
+
+      for (i = 0; i < 10; i++)           @(posedge clk);
+
+                  scan <= 0;             @(posedge clk);
+                             flush <= 1; @(posedge clk);
+                             flush <= 0; @(posedge clk);
+                                         @(posedge clk);
    $stop;
    end
 endmodule
