22915efb4ae56f978d28bc96bfe19b2d9a4efc6b
atangzwj <atangzwj@outlook.com>
2017-05-26 16:51:57 -0700

    Added helloworld_mod.c

---
+++ Lab4/helloworld_mod.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+   alt_putstr("Hello, World!\n");
+
+   char ch = 'a';
+   while (ch != 'G') {
+      ch = alt_getchar();
+   }
+   while(1) {
+      // Insert code here
+   }
+   return 0;
+}
+
