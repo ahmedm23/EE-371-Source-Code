a85894a5934fcfaa6e1a218d6e773abfb1dbb59b
atangzwj <atangzwj@outlook.com>
2017-06-07 18:55:06 -0700

    Added comment in clock16x.sv and added SIPO_SR.sv

---
+++ Lab5/SerialComm/SIPO_SR.sv
@@ -0,0 +1,13 @@
+
+module SIPO_SR (sr_clk, reset, data_out, data_in);
+   output logic [7:0] data_out;
+   input  logic       sr_clk, reset, data_in;
+
+   logic [9:0] buf = 10'b00_0000_0000;
+
+   assign data_out = buf[8:1];
+
+   always_ff @ (posedge sr_clk or posedge reset)
+      if (reset) buf <= 10'b00_0000_0000;
+      else       buf <= {data_in, buf[9:1]};
+endmodule
--- Lab5/SerialComm/clock16x.sv
+++ Lab5/SerialComm/clock16x.sv
@@ -7,8 +7,8 @@ module clock16x (CLOCK_50, clk16x);
 
    always_ff @ (posedge CLOCK_50)
       if (count == 8'b1010_0010) begin
-         count <= 8'b0000_0000;
-         clk16x <= ~clk16x;
+         count <= 8'b0000_0000; // Count to 162 on 50 MHz clk to get half of
+         clk16x <= ~clk16x;     // 16x 9600 bps clk period
       end
       else begin
          count <= count + 1'b1;
