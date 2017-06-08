a224adb526280b219bdb4c773fa11716cce65d58
atangzwj <atangzwj@outlook.com>
2017-06-07 21:37:02 -0700

    clock16x module has been tested

--- Lab5/SerialComm/clock16x.sv
+++ Lab5/SerialComm/clock16x.sv
@@ -1,7 +1,7 @@
 
 module clock16x (CLOCK_50, clk16x);
    input  logic CLOCK_50;
-   output logic clk16x;
+   output logic clk16x = 0;
 
    logic [7:0] count = 8'b0000_0000;
 
@@ -12,6 +12,24 @@ module clock16x (CLOCK_50, clk16x);
       end
       else begin
          count <= count + 1'b1;
-         //clk16x <= clk16x;
       end
+endmodule
+
+module clock16x_testbench ();
+   logic CLOCK_50;
+   logic clk16x;
+
+   clock16x dut (.CLOCK_50, .clk16x);
+
+   parameter CLOCK_PERIOD = 10;
+   initial begin
+      CLOCK_50 <= 0;
+      forever #(CLOCK_PERIOD / 2) CLOCK_50 <= ~CLOCK_50;
+   end
+
+   integer i;
+   initial begin
+      for (i = 0; i < 1000; i++) @(posedge CLOCK_50);
+      $stop;
+   end
 endmodule
