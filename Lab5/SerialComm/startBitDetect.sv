12538512f719c16abb23f164985a3973caf89dcc
atangzwj <atangzwj@outlook.com>
2017-06-07 22:25:06 -0700

    Revised startBitDetect.sv

--- Lab5/SerialComm/startBitDetect.sv
+++ Lab5/SerialComm/startBitDetect.sv
@@ -1,11 +1,10 @@
 
-module startBitDetect(reset, enable, data, char_received);
-   input  logic reset;
-   output logic enable;
+module startBitDetect(enable, data, char_received);
+   output logic enable = 1'b0;
    input  logic data, char_received;
 
-   always_ff @ (posedge reset | negedge data | posedge char_received)
-      if (reset | char_received) enable <= 0;
-      else                       enable <= 1;
+   always_ff @ (negedge data or posedge char_received)
+      if (char_received) enable <= 0;
+      else               enable <= 1;
 endmodule
 
