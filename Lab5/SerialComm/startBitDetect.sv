0b16394368656d621d38eb6c54eb80f3bc199143
atangzwj <atangzwj@outlook.com>
2017-06-07 22:31:37 -0700

    Wrote testbench for startBitDetect

--- Lab5/SerialComm/startBitDetect.sv
+++ Lab5/SerialComm/startBitDetect.sv
@@ -1,10 +1,24 @@
 
-module startBitDetect(enable, data, char_received);
+module startBitDetect (enable, data, char_received);
    output logic enable = 1'b0;
    input  logic data, char_received;
 
    always_ff @ (negedge data or posedge char_received)
       if (char_received) enable <= 0;
       else               enable <= 1;
+endmodule
+
+module startBitDetect_testbench ();
+   logic enable;
+   logic data, char_received;
+
+   startBitDetect dut (.enable, .data, .char_received);
+
+   initial begin
+      data <= 1; char_received <= 0; #20;
+      data <= 0;                     #90;
+      data <= 1;                     #30;
+                 char_received <= 1; #10;
+   end
 endmodule
 
