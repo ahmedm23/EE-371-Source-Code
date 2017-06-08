5df72e52dc4bc49aeadffc6b424ed52725baa0d9
atangzwj <atangzwj@outlook.com>
2017-06-08 13:22:16 -0700

    Changed variable name in startBitDetect.sv

--- Lab5/SerialComm/startBitDetect.sv
+++ Lab5/SerialComm/startBitDetect.sv
@@ -1,24 +1,24 @@
 
-module startBitDetect (enable, data, char_received);
+module startBitDetect (enable, data, char_complete);
    output logic enable = 1'b0;
-   input  logic data, char_received;
+   input  logic data, char_complete;
 
-   always_ff @ (negedge data or posedge char_received)
-      if (char_received) enable <= 0;
+   always_ff @ (negedge data or posedge char_complete)
+      if (char_complete) enable <= 0;
       else               enable <= 1;
 endmodule
 
 module startBitDetect_testbench ();
    logic enable;
-   logic data, char_received;
+   logic data, char_complete;
 
-   startBitDetect dut (.enable, .data, .char_received);
+   startBitDetect dut (.enable, .data, .char_complete);
 
    initial begin
-      data <= 1; char_received <= 0; #20;
+      data <= 1; char_complete <= 0; #20;
       data <= 0;                     #90;
       data <= 1;                     #30;
-                 char_received <= 1; #10;
+                 char_complete <= 1; #10;
       $stop;
    end
 endmodule
